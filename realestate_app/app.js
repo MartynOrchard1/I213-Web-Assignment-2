75c7d8f87fc0c3355c76cb9108ff841de4faa850
Marty <martorchard@gmail.com>
2024-09-07 17:36:07 +1200

    File struc and setup

---
+++ realestate_app/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mysql = require('mysql');
+const app = express();
+const port = 3000;
+
+// Database connection
---
+++ realestate_app/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ['./views/**/*.handlebars'],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
