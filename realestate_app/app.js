7f7a0b6af43cbfd6491acfb286568e4e44c55492
Marty <martorchard@gmail.com>
2024-09-07 17:40:22 +1200

    App.js and create index.js

--- realestate_app/app.js
+++ realestate_app/app.js
@@ -4,4 +4,33 @@ const mysql = require('mysql');
 const app = express();
 const port = 3000;
 
-// Database connection
+// Connect To DB
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'realestate'
+});
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MYSQL');
+});
+
+// Setup handlebars
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Stat Folder
+app.use(express.static('public'));
+
+// Parse To Body
+app.use(express.urlencoded({ extended: true }));
+
+// Setup The Routes
+app.use('/', require('./routes/index.js'));
+
+// PORT 
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
