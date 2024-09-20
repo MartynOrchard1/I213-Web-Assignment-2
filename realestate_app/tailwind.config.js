db1fe7ee78f295593850f78b137a3733c54afacb
Marty <martorchard@gmail.com>
2024-09-20 18:50:57 +1200

    Fix styling path

--- realestate_app/tailwind.config.js
+++ realestate_app/tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-    content: ['./views/**/*.handlebars'],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
+  content: [
+    './realestate_app/views/**/*.', // Handlebar templates
+    './realestate_app/routes/**/*.js',        // Your JS files
+    './realestate_app/public/**/*.html',      // If you have HTML files in public
+    './realestate_app/**/*.js',               // Any JS files throughout the app
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
