331629e3f299de703cff3a85660da7b589562644
Marty <martorchard@gmail.com>
2024-09-22 13:06:56 +1200

    styling works now

--- realestate_app/tailwind.config.js
+++ realestate_app/tailwind.config.js
@@ -1,13 +1,11 @@
 module.exports = {
   content: [
-    './realestate_app/views/home.handlebars',       // Home template
-    './realestate_app/views/layouts/main.handlebars', // Main layout
-    './realestate_app/views/login.handlebars',       // Login template
-    './realestate_app/views/partials/dashboard.handlebars', // Dashboard partial
-    './realestate_app/routes/**/*.js',              // All your JS files in routes folder
+    './views/**/*.handlebars', 
+    './public/**/*.js',
+    './routes/**/*.js',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
