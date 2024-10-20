849fe927edd6acccbde22c8c840f33a1bd21c3f2
Marty <martorchard@gmail.com>
2024-10-20 20:42:22 +1300

    Update prop.js

--- realestate_app/routes/prop.js
+++ realestate_app/routes/prop.js
@@ -4,9 +4,14 @@ const Property = require('../models/property');
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../db');
 
-
-
-
+// Route: Properties.handlebars
+router.get('/properties', (req, res) => {
+    if (req.session.user) {
+        res.render('properties', { user: req.sesion.user, layout: false, title: "Properties"});
+    } else {
+        res.redirect('/login');
+    }
+});
 
 
 
