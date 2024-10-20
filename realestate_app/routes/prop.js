59536745014bcda541de81f0ff59d1c238202f1e
Marty <martorchard@gmail.com>
2024-10-20 20:29:53 +1300

    css shit

---
+++ realestate_app/routes/prop.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const Property = require('../models/property');
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+
+
+
+
+
+
+module.exports = router;
