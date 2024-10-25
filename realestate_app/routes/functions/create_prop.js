ff33f1f5dbb8c0bfd8190d0c51e8d8aa7204c1b9
Marty <martorchard@gmail.com>
2024-10-26 12:48:48 +1300

    Add functions

--- realestate_app/routes/functions/create_prop.js
+++ realestate_app/routes/functions/create_prop.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Property = require('../models/property'); 
+const Property = require('../../models/property'); 
 const { error } = require('console');
 const multer = require('multer');
 
