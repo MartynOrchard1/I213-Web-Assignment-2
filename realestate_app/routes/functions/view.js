5d27b5ed086baf9f105ef353fab04338e36856bd
Marty <martorchard@gmail.com>
2024-10-28 11:15:46 +1300

    show file

---
+++ realestate_app/routes/functions/view.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const Property = require('../../models/property');
+
+// GET route: Render the view for a property by ID
+router.get("/view/:id", async (req, res) => {
+    if (req.session.user) {
+        try {
+            const property = await Property.findByPk(req.params.id);
+            if (!property) {
+                return res.status(404).send("Property not found");
+            }
+            const plainProperty = property.get({ plain: true });
+            plainProperty.image_url = `/images/houses/${plainProperty.image_name}`; // Adjust as per your structure
+
+            res.render("properties/show", {
+                layout: "main",
+                title: "View Property",
+                property: plainProperty
+            });
+        } catch (error) {
+            console.error('Error fetching property:', error);
+            res.status(500).send('An error occurred while fetching the property');
+        }
+    } else {
+        res.redirect('/login');
+    }
+});
+
+// Export
+module.exports = router;
