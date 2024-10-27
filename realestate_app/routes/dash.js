be954f37894731fdd83fa926b2789947d626771f
Marty <martorchard@gmail.com>
2024-10-28 10:47:17 +1300

    Pull new id's

--- realestate_app/routes/dash.js
+++ realestate_app/routes/dash.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Property = require('../models/property'); 
+const { Op } = require('sequelize');
 
 // Protected Route: Dashboard
 router.get('/dashboard', async (req, res) => {
@@ -8,10 +9,15 @@ router.get('/dashboard', async (req, res) => {
         try {
             const properties = await Property.findAll({
                 where: {
-                    id: [6897, 6882, 6881, 6880, 6879, 6878, 6877, 6876, 6875, 6874, 6873] // Array of id's to pull from DB
+                    [Op.or]: [
+                        { id: { [Op.gt]: 6882 } },
+                        { id: { [Op.between]: [6873, 6882] } }
+                    ]
+                    // id: [6897, 6882, 6881, 6880, 6879, 6878, 6877, 6876, 6875, 6874, 6873] // Array of id's to pull from DB !! KEEP THIS FOR TESTING PURPOSES !!
                 },
                 order: [['id', 'DESC']] // Sort in descending order | Change 'DESC' to 'ASC' if you wish it to be ascending order
             });
+            
             res.render('dashboard', {
                 user: req.session.user,
                 properties: properties.map(p => p.toJSON()), // Ensure properties are serialized for Handlebars
