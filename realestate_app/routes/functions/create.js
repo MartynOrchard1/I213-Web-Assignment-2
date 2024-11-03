7d13638517da5e0a3f4f624944710832c9e96789
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2024-11-04 09:35:30 +1300

    Update create.js

--- realestate_app/routes/functions/create.js
+++ realestate_app/routes/functions/create.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Property = require('../../models/property'); 
-const { error } = require('console');
 const multer = require('multer');
 
 // GET route: create.handlebars
